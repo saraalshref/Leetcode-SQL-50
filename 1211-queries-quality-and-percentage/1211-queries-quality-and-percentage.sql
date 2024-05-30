2e73b21ce7fcecf864e64ef925d977b28c413cc7
Sara mohamed <148913372+saraalshref@users.noreply.github.com>
2024-05-30 12:16:03 +0300

    Time: 728 ms (49.90%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,7 +1,7 @@
 
 SELECT 
     query_name,
-    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
+    ROUND(AVG(rating  / position), 2) AS quality,
     ROUND(100.0 * SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*), 2) AS poor_query_percentage
 FROM 
     Queries 
